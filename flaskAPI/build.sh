b76d86b03b5bff268d9063161b0418e0ee8cabb2
Josue049 <josue09guti@gmail.com>
2025-03-20 00:49:11 -0500

    install ffmpeg agregado

--- flaskAPI/build.sh
+++ flaskAPI/build.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+set -e  # Detiene la ejecución si hay algún error
+
+# Instalar dependencias del sistema
+apt-get update
+apt-get install -y ffmpeg
+
+# Descargar e instalar wkhtmltopdf
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb
 dpkg -i wkhtmltox_0.12.6-1.buster_amd64.deb || true
 apt-get install -f -y
+
+# Verificar que FFmpeg se instaló correctamente
+ffmpeg -version
+
+# Instalar dependencias de Python
 pip install -r requirements.txt
