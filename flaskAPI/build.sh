7b6b5502c4421b8829d983776f18046fd8662401
Josue049 <josue09guti@gmail.com>
2025-03-20 00:55:19 -0500

    update build ffmpeg

--- flaskAPI/build.sh
+++ flaskAPI/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e  # Detiene la ejecución si hay algún error
+# set -e  # Detiene la ejecución si hay algún error
 
 # Instalar dependencias del sistema
-apt-get update
+# apt-get update
 apt-get install -y ffmpeg
 
 # Descargar e instalar wkhtmltopdf
@@ -14,4 +14,10 @@ apt-get install -f -y
 ffmpeg -version
 
 # Instalar dependencias de Python
+pip install -r requirements.txt
+
+
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb
+dpkg -i wkhtmltox_0.12.6-1.buster_amd64.deb || true
+apt-get install -f -y
 pip install -r requirements.txt
