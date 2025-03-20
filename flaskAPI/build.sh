a05bd1320ba061dcde0a164a22f5933865a14cbf
Josue049 <josue09guti@gmail.com>
2025-03-20 01:10:13 -0500

    corrección de dependencias duplicadas

--- flaskAPI/build.sh
+++ flaskAPI/build.sh
@@ -15,9 +15,3 @@ ffmpeg -version
 
 # Instalar dependencias de Python
 pip install -r requirements.txt
-
-
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb
-dpkg -i wkhtmltox_0.12.6-1.buster_amd64.deb || true
-apt-get install -f -y
-pip install -r requirements.txt
