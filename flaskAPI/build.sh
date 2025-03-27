0cc5e3a8b1143cabfd7398755036e9ca688ef7db
Josue049 <josue09guti@gmail.com>
2025-03-27 15:31:49 -0500

    listo para docker

--- flaskAPI/build.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb
-dpkg -i wkhtmltox_0.12.6-1.buster_amd64.deb || true
-apt-get install -f -y
-pip install -r requirements.txt
